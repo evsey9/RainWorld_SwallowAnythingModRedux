1254044530c622ea1a8920e5d42083b5981b44fe
evsey9 <evseyru1@mail.ru>
2023-01-25 09:59:47 +0300

    v1.2

---
+++ ReleaseGraspPatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using On;
+
+
+
+namespace SwallowAnythingMod
+{
+    class ReleaseGraspPatch
+    {
+        public static void Patch()
+        {
+            On.Player.ReleaseGrasp += Player_ReleaseGrasp;
+        }
+
+        private static void Player_ReleaseGrasp(On.Player.orig_ReleaseGrasp orig, Player self, int grasp)
+        {
+            if (self.input[0].pckp == true && self.input[1].pckp == false && self.input[0].y == 0 && self.input[0].thrw == false && (self.Grabability(self.grasps[grasp]?.grabbed) == Player.ObjectGrabability.TwoHands || self.Grabability(self.grasps[grasp]?.grabbed) == Player.ObjectGrabability.Drag))
+            {
+            }
+            else
+            {
+                orig(self, grasp);
+            }
+        }
+    }
+}
---
+++ SwallowAnythingMod.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace SwallowAnythingMod
+{
+    [BepInPlugin("drwoof.swallowanythingmod", "Swallow Anything Mod", "1.1.0")]
+    public class SwallowAnythingMod : BaseUnityPlugin
+    {
+        public SwallowAnythingMod()
+        {
+            CanBeSwallowedPatch.Patch();
+            ReleaseGraspPatch.Patch();
+            SlugOnBackPatch.Patch();
+        }
+    }
+}
